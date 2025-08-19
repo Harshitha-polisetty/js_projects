7d003c27110be0c40319b1e4f01d8bf29110b13c
Harshitha-polisetty <saiharshitha593@gmail.com>
2025-08-19 11:29:18 +0530

    Add files via upload

---
+++ colorpicker/script.js
@@ -0,0 +1,12 @@
+document.body.style.backgroundColor="gold"
+function ccr(){
+    document.body.style.backgroundColor="red"
+}
+function ccb(){
+    document.body.style.backgroundColor="blue"
+}
+function cco(){
+    document.body.style.backgroundColor="orange"
+}
+
+
